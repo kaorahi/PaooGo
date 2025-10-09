f3bda23496545eca33eeb65dc89e8133df575f3e
Kazuma Arino <hogeika2@gmail.com>
2025-10-09 20:31:36 +0900

    Add engine links.

--- android/src/main/java/io/github/karino2/paoogo/ui/AboutActivity.kt
+++ android/src/main/java/io/github/karino2/paoogo/ui/AboutActivity.kt
@@ -37,17 +37,22 @@ class AboutActivity : AppCompatActivity() {
     }
 
     fun getData() = arrayOf(
-        LinkWithDescription("http://ligi.de", "gobandroid", "Ligi"),
+        LinkWithDescription("https://github.com/karino2/PaooGo", "PaooGo source", "karino2"),
+        LinkWithDescription("https://amigogtp.sourceforge.net/", "AmiGoGtp"),
+        LinkWithDescription("https://lists.gnu.org/archive/html/gnugo-devel/2005-08/msg00036.html", "Liberty", "GnuGo fork"),
+        LinkWithDescription("https://github.com/lightvector/KataGo", "KataGo"),
+        LinkWithDescription("https://www.gnu.org/software/gnugo/devel.html", "GnuGo"),
 
+        LinkWithDescription("http://ligi.de", "gobandroid, PaooGo forked from", "Ligi"),
         LinkWithDescription("http://jakewharton.github.io/butterknife/", "Jake Wharton", "ButterKnife"),
         LinkWithDescription("https://developers.google.com/", "Google", "Android, Dagger, .."),
-        LinkWithDescription("http://square.github.io/", "Square", "okhttp, assertj-android"),
+        LinkWithDescription("https://square.github.io/", "Square", "okhttp, assertj-android"),
         LinkWithDescription("https://kotlinlang.org/", "JetBrains Kotlin", "one awesome langueage used"),
         LinkWithDescription("https://github.com/greenrobot", "GreenRobot", "eventbus"),
         LinkWithDescription("https://github.com/N3TWORK/alphanum", "N3TWORK", "Alphanum comparator"),
-        LinkWithDescription("http://jchardet.sourceforge.net/index.html", "jchardet", "jchardet is a java port of the source from mozilla's automatic charset detection algorithm"),
-        LinkWithDescription("http://github.com/Zenigata", "French Translation #2", "Zenigata"),
-        LinkWithDescription("http://github.com/p3l", "Swedish Translation", "Peter Lundqvist"),
+        LinkWithDescription("https://jchardet.sourceforge.net/index.html", "jchardet", "jchardet is a java port of the source from mozilla's automatic charset detection algorithm"),
+        LinkWithDescription("https://github.com/Zenigata", "French Translation #2", "Zenigata"),
+        LinkWithDescription("https://github.com/p3l", "Swedish Translation", "Peter Lundqvist"),
         LinkWithDescription("https://github.com/gthazmatt", "Code contributions", "gthazmatt")
     )
 }
