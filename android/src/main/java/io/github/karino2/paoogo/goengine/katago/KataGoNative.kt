8cc8d191b5b87a55bd9799324c1683aa346cd5ae
Hiraoka <kakkokakko@gmail.com>
2025-11-01 22:30:07 +0900

    Use JSON for analysis response

--- android/src/main/java/io/github/karino2/paoogo/goengine/katago/KataGoNative.kt
+++ android/src/main/java/io/github/karino2/paoogo/goengine/katago/KataGoNative.kt
@@ -10,8 +10,30 @@ import kotlin.math.pow
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 import kotlin.random.Random
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
 import org.ligi.gobandroid_hd.logic.GTPHelper
 import org.ligi.gobandroid_hd.logic.GoGame
+
+// (cf.) https://github.com/lightvector/KataGo/blob/master/docs/Analysis_Engine.md#responses
+@Serializable
+data class MoveInfo(
+    val move: String,
+    val order: Int,
+    val prior: Double,
+    val pv: List<String>,
+    val scoreLead: Double,
+    val scoreStdev: Double,
+    val visits: Int,
+    val winrate: Double,
+)
+
+@Serializable
+data class AnalysisResponse(
+    val moveInfos: List<MoveInfo>,
+)
 
 class KataGoNative : GoEngine, GoAnalyzer {
     companion object {
@@ -35,33 +57,29 @@ class KataGoNative : GoEngine, GoAnalyzer {
 
     override external fun doPass(isBlack: Boolean)
 
-    // based on lz-analyze.
-    // [KataGo/docs/GTP_Extensions.md at master · lightvector/KataGo](https://github.com/lightvector/KataGo/blob/master/docs/GTP_Extensions.md)
-    // winrate: [0, 10000]
-    // F4 5506 is the candidate and winrate. After it, pv is followed.
-    // each info is separate by ",".
-    // example is following.
-    // F4 5506 F4 E2 G4 E7 F7 H3,D6 5546 D6 D7 C7 E6 D5 E7 E5 C8 B7 B8 B6,D7 5341 D7 D6 E6 C7 D8 B4,G5 5099 G5 D2 D7 C7 C3,C5 5092 C5 D7 F4 H5,E3 4829 E3 H5 H6 G5 G6 D7,E7 4955 E7 D7 F4 E2,E5 4706 E5 B4 H4,B5 4847 B5 E5,G4 4320 G4,F3 4450 F3,E2 4238 E2,C7 4212 C7,H5 4164 H5
+    // based on KataGo's analysis command
+    // https://github.com/lightvector/KataGo/blob/master/docs/Analysis_Engine.md#responses
     external fun analyze(msec: Int, isBlack: Boolean) : String
+
+    private val json = Json { ignoreUnknownKeys = true }
+    private fun parseAnalysisResponse(res: String) : AnalysisResponse {
+        return json.decodeFromString<AnalysisResponse>(res)
+        // return json.decodeFromString(serializer<AnalysisResponse>(), res)
+    }
 
     override fun analyzeSituation(isBlack: Boolean, game: GoGame): List<AnalyzeInfo> {
         val res = analyze(2000, isBlack)
         println(res)
         if (res.isEmpty())
             return emptyList()
-        return res.split("\n")
-            .filterNot { it.isEmpty() }
-            .last()
-            .split(",")
-            .mapNotNull {
-                val arr = it.split(" ")
-                if (arr.size <= 2)
+        val moveInfos = parseAnalysisResponse(res).moveInfos
+        return moveInfos.mapNotNull {
+                val move = it.move
+                if (move.uppercase() == "PASS")
                     return@mapNotNull null
-                if (arr[0].uppercase() == "PASS")
-                    return@mapNotNull null
-                val pos = MovePos.fromString(arr[0], game)
-                val rate = arr[1].toDouble()/(10000.0)
-                val pv = arr.subList(2, arr.size).mapNotNull {
+                val pos = MovePos.fromString(move, game)
+                val rate = it.winrate
+                val pv = it.pv.mapNotNull {
                     if (it.uppercase() == "PASS") null else GTPHelper.strToCell(it, game)
                 }
                 AnalyzeInfo(game.visualBoard.getCell(pos.x, pos.y), rate, pv)
@@ -73,18 +91,11 @@ class KataGoNative : GoEngine, GoAnalyzer {
         println(res)
         if (res.isEmpty())
             return MovePos.PASS
-        val arr = res.split("\n")
-            .filterNot { it.isEmpty() }
-            .last()
-            .split(",")
-            .first()
-            .split(" ")
-        if (arr.size <= 2)
+        val moveInfos = parseAnalysisResponse(res).moveInfos
+        val bestMove = moveInfos.minByOrNull { it.order }?.move
+        if (bestMove == null || bestMove.uppercase() == "PASS")
             return MovePos.PASS
-        if (arr[0].uppercase() == "PASS")
-            return MovePos.PASS
-
-        return MovePos.fromString(arr[0], game)
+        return MovePos.fromString(bestMove, game)
     }
 
     private fun genMoveInternalUsingRawNN(isBlack: Boolean) : Int {
